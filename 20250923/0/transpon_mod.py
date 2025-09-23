d4280a67b7ec7e6fd68fb2872c86407ef30891b6
Max Big <maxbig@MacBook-Air-Max.local>
2025-09-23 11:47:37 +0300

    better

--- 20250923/0/transpon_mod.py
+++ 20250923/0/transpon_mod.py
@@ -1,7 +1,7 @@
 matrix = []
 while s := input():
     matrix.append(list(eval(s)))
-if len(matrix) == len(matrix[0]) and all(len(matrix[i]) == len(matrix[i + 1]) for i in range(len(matrix) - 1)):
+if all(len(matrix[i]) == len(matrix) for i in range(len(matrix))):
     print('matrix is square')
     for i in range(len(matrix)):
         for j in range(i + 1, len(matrix)):
