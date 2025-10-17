18a371890b5885336e9463caf2a8ad24c8c35b6e
Max Big <maxbig@MacBook-Air-Max.local>
2025-10-18 02:09:51 +0300

    popular w-len-words

---
+++ 20251014/3/prog.py
@@ -0,0 +1,25 @@
+w = int(input())
+s = input()
+word = ''
+dct = {}
+while len(s) != 1:
+    for sym in s:
+        if sym.isalpha():
+            word += sym.lower()
+        else:
+            if len(word) == w:
+                if word not in dct.keys():
+                    dct[word] = 1
+                else:
+                    dct[word] += 1
+            word = ''
+    s = input()
+max_words = []
+max_count = 0
+for word in dct.keys():
+    if dct[word] > max_count:
+        max_count = dct[word]
+        max_words = [word]
+    elif dct[word] == max_count:
+        max_words.append(word)
+print(*max_words)
